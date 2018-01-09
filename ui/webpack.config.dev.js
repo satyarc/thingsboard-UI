64d8eb8ac52d94473ca93dc85cfe87065102c9c5
satyarc <satyarchandra@gmail.com>
2018-01-09 15:56:47 +0530

    Changed the logo and title

--- ui/webpack.config.dev.js
+++ ui/webpack.config.dev.js
@@ -45,13 +45,13 @@ module.exports = {
             moment: "moment"
         }),
         new CopyWebpackPlugin([
-            { from: './src/thingsboard.ico', to: 'thingsboard.ico' }
+            { from: './src/intellipredikt.ico', to: 'intellipredikt.ico' }
         ]),
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
-            title: 'ThingsBoard',
+            title: 'Intellipredikt',
             inject: 'body',
         }),
         new webpack.optimize.OccurrenceOrderPlugin(),
